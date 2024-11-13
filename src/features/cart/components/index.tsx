999e837ab96b68c7e75935b2d9b55377b1bbcd6c
Mohammad <bhshtbaran@gmail.com>
2024-11-13 08:54:30 -0800

    chore: changing some issues

--- src/features/cart/components/index.tsx
+++ src/features/cart/components/index.tsx
@@ -7,7 +7,7 @@ import MainContainer from "@/components/partials/containers/main-container";
 import useCart from "@/core/hooks/use-cart.hooks";
 
 const Cart = () => {
-  const cart = useCart();
+  const cartItems = useCart((state) => state.items);
 
   return (
     <div className="bg-white">
@@ -16,11 +16,11 @@ const Cart = () => {
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
-              {cart.items.length === 0 && (
+              {cartItems.length === 0 && (
                 <p className="text-neutral-500">No items added to cart</p>
               )}
               <ul>
-                {cart.items.map((item) => (
+                {cartItems.map((item) => (
                   <CartItem key={item.id} data={item} />
                 ))}
               </ul>
