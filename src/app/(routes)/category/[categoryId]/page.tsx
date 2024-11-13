1714870a74af3a26e87b63b70f8e2b97897aaa57
Mohammad <bhshtbaran@gmail.com>
2024-11-13 09:50:24 -0800

    feat: add metadata

--- src/app/(routes)/category/[categoryId]/page.tsx
+++ src/app/(routes)/category/[categoryId]/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from "next";
+
 import Category from "@/features/category/components";
 import { getCategory } from "@/features/category/core/services/api";
 import getColors from "@/features/colors/core/services/api";
@@ -36,5 +38,23 @@ const CategoryPage = async ({ params, searchParams }: ICategoryPageProps) => {
     />
   );
 };
+
+export async function generateMetadata({
+  params,
+}: ICategoryPageProps): Promise<Metadata> {
+  const { categoryId } = await params;
+
+  const category = await getCategory(categoryId);
+
+  const billboardImage = category?.billboard.imageUrl ?? "/placeholder.jpeg";
+
+  return {
+    title: category?.name,
+    description: `On this page, you can see the billboard and products of '${category?.name}'`,
+    openGraph: {
+      images: [billboardImage],
+    },
+  };
+}
 
 export default CategoryPage;
